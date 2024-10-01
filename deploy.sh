c196434070a3662760a05f3038ec746817c74cb0
AruzhanShinbayeva <a.shinbaeyeva@innopolis.university>
2024-10-02 01:50:41 +0300

    try build and deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 sudo docker system prune --all -f
-sudo docker compose down
-sudo docker compose pull
-sudo docker compose up -d
+sudo docker-compose down
+sudo docker-compose pull
+sudo docker-compose up -d
